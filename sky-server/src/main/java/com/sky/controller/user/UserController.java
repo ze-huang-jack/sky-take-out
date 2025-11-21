b7e5b784f906dbb2ae4b737e1a98c9350de3588c
ze huang(jack) <suqiuliang727@gmail.com>
2025-11-22 00:45:12 +0800

    add UserLoginDTO & UserLoginVO

---
+++ sky-server/src/main/java/com/sky/controller/user/UserController.java
@@ -0,0 +1,13 @@
+package com.sky.controller.user;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/user/user")
+public class UserController {
+
+
+}
